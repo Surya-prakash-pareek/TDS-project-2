6111f2c114d6d2ba6ca587a01206e080310676a6
Surya Prakash Pareek <139848993+Surya-prakash-pareek@users.noreply.github.com>
2025-08-13 14:59:13 +0530

    Update task_engine.py

--- task_engine.py
+++ task_engine.py
@@ -21,12 +21,12 @@ async def run_python_code(code: str, libraries: List[str], folder: str = "upload
         exec_globals = {}
         exec(code, exec_globals)
 
-    # Step 1: Install all required libraries first
-    for lib in libraries:
-        try:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", lib])
-        except Exception as install_error:
-            return {"code": 0, "output":f"❌ Failed to install library '{lib}':\n{install_error}"}
+    # # Step 1: Install all required libraries first
+    # for lib in libraries:
+    #     try:
+    #         subprocess.check_call([sys.executable, "-m", "pip", "install", lib])
+    #     except Exception as install_error:
+    #         return {"code": 0, "output":f"❌ Failed to install library '{lib}':\n{install_error}"}
 
     # Step 2: Execute the code after installation
     try:
